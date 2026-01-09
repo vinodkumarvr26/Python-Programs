531dc6e200451ec63dc2a01f7400966f6b3c621c
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-01-09 21:25:53 +0530

    Add files via upload

---
+++ Arithemetic_Mean1.py
@@ -0,0 +1,5 @@
+#Write a program that calculates the arithmetic mean of two numbers.
+num1= float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+mean= (num1 + num2) / 2
+print("Arithmetic mean:", mean)
