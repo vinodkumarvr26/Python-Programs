08898d4b1f9ffbb07d876b80a9c2f809ba4c2837
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-01-09 21:26:22 +0530

    Add files via upload

---
+++ Arithemetic_Mean2.py
@@ -0,0 +1,6 @@
+#Create a program that calculates and displays the arithmetic mean of three grades entered by the user.
+grade1= float(input("Enter the first grade: "))
+grade2= float(input("Enter the second grade: "))
+grade3 = float(input("Enter the third grade: "))
+mean =(grade1 + grade2+ grade3) / 3
+print("Arithmetic mean:", mean)
