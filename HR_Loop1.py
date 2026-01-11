3f34761503452f8282e90cbe6843bfcf3cdc3ef4
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-01-11 16:32:37 +0530

    Add files via upload

---
+++ HR_Loop1.py
@@ -0,0 +1,8 @@
+#Task
+#The provided code stub reads an integer. n, from STDIN. For all non-negative integers i < n. print i².
+#Example n=3
+#The list of non-negative integers that are less than n = 3 is [0, 1, 2]. Print the square of each number on a separate line.
+n = int(input())
+for i in range(n):
+    sq=i*i
+    print(sq)
