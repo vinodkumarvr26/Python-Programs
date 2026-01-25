a5af0bbbf03f8cc0c33c6a92e206100e8cc4253d
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-01-25 16:34:52 +0530

    Add files via upload

---
+++ HR_Loop2.py
@@ -0,0 +1,7 @@
+#The included code stub will read an integer, , from STDIN.
+#Without using any string methods, try to print the following:
+#123...n
+#Note that "" represents the consecutive values in between
+n = int(input())
+for i in range(1,n+1):
+    print(i,end="")
