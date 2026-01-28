c9a9b34723ef0a91b6b13e381830bf470da0820a
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-01-28 20:39:36 +0530

    Update Swap_numbers.py

--- Swap_numbers.py
+++ Swap_numbers.py
@@ -1,4 +1,4 @@
-gt
+
 a = input("Enter the value of the first variable (a): ")
 b = input("Enter the value of the second variable (b): ")
 print(f"Original values: a = {a}, b = {b}")
@@ -6,3 +6,4 @@ temp = a
 a = b
 b = temp
 print(f"Swapped values: a = {a}, b = {b}")
+
