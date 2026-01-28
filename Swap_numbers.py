1ebe725303aea607af7b370840ec6c0e52bfd4f0
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-01-28 20:39:22 +0530

    Update Swap_numbers.py

--- Swap_numbers.py
+++ Swap_numbers.py
@@ -1,12 +1,8 @@
-#Write a Python program to swap two variables.
-# Input two variables
+gt
 a = input("Enter the value of the first variable (a): ")
 b = input("Enter the value of the second variable (b): ")
-# Display the original values
 print(f"Original values: a = {a}, b = {b}")
-# Swap the values using a temporary variable
 temp = a
 a = b
 b = temp
-# Display the swapped values
 print(f"Swapped values: a = {a}, b = {b}")
