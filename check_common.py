fb0a01734751234e56ab69d4d60d9101dfecdef8
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-02-10 21:53:54 +0530

    Add files via upload

---
+++ check_common.py
@@ -0,0 +1,4 @@
+python_course = {"Asha", "Ravi", "John", "Meena"}
+data_science = {"John", "Meena", "Kiran", "Ravi"}
+common_students = python_course.intersection(data_science)
+print("Students enrolled in both courses:", common_students)
