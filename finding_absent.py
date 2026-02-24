701424458b22f29dca0d27efcc9dde7549d5344e
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-02-24 22:31:47 +0530

    Add files via upload

---
+++ finding_absent.py
@@ -0,0 +1,4 @@
+all_students = {"Asha", "Ravi", "John", "Meena", "Kiran"}
+present_students = {"John", "Meena", "Ravi"}
+absent_students = all_students.difference(present_students)
+print("Absent students:", absent_students)
