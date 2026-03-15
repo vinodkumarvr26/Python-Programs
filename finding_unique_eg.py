e9d57945c4bc0dcfd2779def963c14694e7defbb
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-03-15 21:04:06 +0530

    Update coding club members and CS department

--- finding_unique_eg.py
+++ finding_unique_eg.py
@@ -1,4 +1,4 @@
-cs_department = {"Asha", "Ravi", "John", "Meena", "Kiran"}
-coding_club = {"Ravi", "John"}
+cs_department = {"Asha", "Ravi", "John", "Meena", "Kiran","Kumar"}
+coding_club = {"Ravi", "John","Kumar"}
 print("All club members are from CS:", coding_club.issubset(cs_department))
 print("CS dept contains all club members:", cs_department.issuperset(coding_club))
