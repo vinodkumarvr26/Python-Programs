7a7758f324e2a180bca6edd5a70c30dbb52be4e1
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-03-17 20:49:26 +0530

    Add 'Priya' to python_course set

--- check_common.py
+++ check_common.py
@@ -1,4 +1,4 @@
-python_course = {"Asha", "Ravi", "John", "Meena"}
+python_course = {"Asha", "Ravi", "John", "Meena","Priya"}
 data_science = {"John", "Meena", "Kiran", "Ravi"}
 common_students = python_course.intersection(data_science)
 print("Students enrolled in both courses:", common_students)
