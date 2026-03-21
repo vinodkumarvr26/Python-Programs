992f4b265b533211c51b1ff5a55b55fa0aec53cf
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-03-21 23:19:13 +0530

    Implement pronic number checker and printer using python
    Add function to check for pronic numbers and print them.

---
+++ pronic_numbers.py
@@ -0,0 +1,9 @@
+def is_pronic_number(num):
+  for n in range(1, int(num**0.5) + 1):
+    if n * (n + 1) == num:
+      return True
+  return False
+print("Pronic numbers between 1 and 100 are:")
+for i in range(1, 101):
+  if is_pronic_number(i):
+    print(i, end=" | ")
