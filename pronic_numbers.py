6360a838d5660a75eb16cfccf78235aaa8812989
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-03-26 05:52:06 +0530

    Fix print statement for pronic numbers output

--- pronic_numbers.py
+++ pronic_numbers.py
@@ -3,7 +3,8 @@ def is_pronic_number(num):
     if n * (n + 1) == num:
       return True
   return False
-print("Pronic numbers between 1 and 100 are:")
+  
+print("Pronic Numbers between 1 and 100 are : ")
 for i in range(1, 101):
   if is_pronic_number(i):
     print(i, end=" | ")
