fbedfa8285a5b88c740bbc594225d6a8e2b70c85
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-04-12 22:53:05 +0530

    Fix indentation and formatting in arithmetic mean calculation

--- Arithemetic_Mean1.py
+++ Arithemetic_Mean1.py
@@ -1,4 +1,3 @@
-#Write a program that calculates the arithmetic mean of two numbers.
 num1= float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 mean= (num1 + num2) / 2
