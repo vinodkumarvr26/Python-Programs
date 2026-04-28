10408eaec2e318ba086e700a1407f5c55127723c
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-04-28 18:39:45 +0530

    Remove unnecessary comments from largest4.py

--- largest4.py
+++ largest4.py
@@ -1,10 +1,6 @@
-# Program to find the largest number in a list
-
 numbers = [12, 45, 7, 89, 23]
 largest = numbers[0]
-
 for num in numbers:
     if num > largest:
         largest = num
-
 print("Largest number in the list:", largest)
