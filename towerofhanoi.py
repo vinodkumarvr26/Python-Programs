fcd14983a5d553eb437e47cc5880778733cf397d
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-05-10 21:21:30 +0530

    Refactor tower of Hanoi implementation

--- towerofhanoi.py
+++ towerofhanoi.py
@@ -2,7 +2,6 @@ n = 4
 src = 'A'
 des = 'B'
 aux = 'C'
-
 def move_disks(n, src, des, aux):
     if n == 1:
         print(f"Move disk 1 from {src} to {des}")
@@ -10,5 +9,4 @@ def move_disks(n, src, des, aux):
     move_disks(n - 1, src, aux, des)
     print(f"Move disk {n} from {src} to {des}")
     move_disks(n - 1, aux, des, src)
-
 move_disks(n, src, des, aux)
