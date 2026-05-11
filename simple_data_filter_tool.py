41e32437686093360d198f6044e4cf63ce95a628
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-05-11 19:34:06 +0530

    Fix print statement for count of passed students

--- simple_data_filter_tool.py
+++ simple_data_filter_tool.py
@@ -1,13 +1,8 @@
 import pandas as pd
-
 data = {"Name": ["A", "B", "C", "D"],
         "Marks": [85, 40, 75, 30]}
-
 df = pd.DataFrame(data)
-
 passed = df[df["Marks"] >= 50]
-
 print("All Data:\n", df)
 print("\nPassed Students:\n", passed)
-
 print("Count Passed:", len(passed))
