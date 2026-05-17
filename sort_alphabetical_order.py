5f4b00b00fb5af6fbfa248772196c942169dfdfc
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-05-17 22:59:14 +0530

    Refactor sorting logic and clean up comments
    Removed comments and improved word capitalization.

--- sort_alphabetical_order.py
+++ sort_alphabetical_order.py
@@ -1,6 +1,4 @@
-# Program to sort alphabetically the words form a string provided by th
 my_str = input("Enter a string: ")
-# breakdown the string into a list of words
 words = [word.capitalize() for word in my_str.split()]
 # sort the list
 words.sort()
