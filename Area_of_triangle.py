4aa741143c52e23e14c34b072143e833cd848173
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-05-23 16:08:10 +0530

    Refactor area calculation and output display

--- Area_of_triangle.py
+++ Area_of_triangle.py
@@ -1,6 +1,4 @@
 base = float(input("Enter the length of the base of the triangle: "))
 height = float(input("Enter the height of the triangle: "))
 area = 0.5 * base * height
-# Display the result
-
 print(f"The area of the triangle is: {area}")
