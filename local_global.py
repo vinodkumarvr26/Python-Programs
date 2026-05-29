c76653878be0c7c62c304df19cc5b0a06d28fc15
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-05-29 22:32:51 +0530

    Fix formatting issues in local_global.py

--- local_global.py
+++ local_global.py
@@ -3,6 +3,5 @@ def show():
     x = 20   # local variable with same name
     print("Local x inside function =", x)
     print("Global x inside function =", globals()['x'])
-# main program or driver code
 show()
 print("global variable outside function x =", x)
