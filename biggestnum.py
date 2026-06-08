08c48a8f7f74704bb608ef249736cf70a491a4bc
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-06-08 21:08:35 +0530

    Add files via upload

---
+++ biggestnum.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=int(input("Enter third number"))
+if a>b and a>c:
+    print(a,"is biggest")
+elif b>c:
+    print(b,"is biggest")
+else:
+    print(c,"is biggest")
+print("End of the program")
