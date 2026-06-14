122f03b3a0f6fdae3973674adf9edab73bec6c58
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-06-14 22:44:08 +0530

    Clean up comments in check_LeapYear.py
    Removed comments explaining leap year logic.

--- check_LeapYear.py
+++ check_LeapYear.py
@@ -1,7 +1,4 @@
 year = int(input("Enter a year: "))
-
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
 if (year % 400 == 0) and (year % 100 == 0):
  print("{0} is a leap year".format(year))
 
