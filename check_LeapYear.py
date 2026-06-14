21415c77509cbba0aebdb98dc195495a9af58e8c
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-06-14 22:44:27 +0530

    Clean up comments in check_LeapYear.py
    Removed comments explaining leap year conditions.

--- check_LeapYear.py
+++ check_LeapYear.py
@@ -1,9 +1,6 @@
 year = int(input("Enter a year: "))
 if (year % 400 == 0) and (year % 100 == 0):
  print("{0} is a leap year".format(year))
-
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
 elif (year % 4 ==0) and (year % 100 != 0):
  print("{0} is a leap year".format(year))
 
