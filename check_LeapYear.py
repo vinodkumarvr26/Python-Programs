8c9d909fb973546499f255a926db6732f30cbb9d
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-06-14 22:44:49 +0530

    Refactor leap year check and remove commented code

--- check_LeapYear.py
+++ check_LeapYear.py
@@ -3,9 +3,5 @@ if (year % 400 == 0) and (year % 100 == 0):
  print("{0} is a leap year".format(year))
 elif (year % 4 ==0) and (year % 100 != 0):
  print("{0} is a leap year".format(year))
-
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
 else:
  print("{0} is not a leap year".format(year))
-
