cb083efe1d04fb249b3c08816383b29beef9c68c
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-06-19 10:53:04 +0530

    Add print statement for updated OrderedDict
    Print the updated OrderedDict after adding a new item.

--- begining_insertion_orderedDict.py
+++ begining_insertion_orderedDict.py
@@ -3,5 +3,4 @@ ordered_dict = OrderedDict([('b', 2), ('c', 3), ('d', 4)])
 new_item = ('a', 1)
 new_ordered_dict = OrderedDict([new_item])
 new_ordered_dict.update(ordered_dict)
-# Print the updated OrderedDict
 print("Updated OrderedDict:", new_ordered_dict)
