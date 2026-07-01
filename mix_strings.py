6fe64e52ca48871babcd8690a0752e2e13944f89
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-07-01 22:10:11 +0530

    Add files via upload

---
+++ mix_strings.py
@@ -0,0 +1,23 @@
+s1 = "Abc"
+s2 = "Xyz"
+
+# get string length
+s1_length = len(s1)
+s2_length = len(s2)
+
+# get length of a bigger string
+length = s1_length if s1_length > s2_length else s2_length
+result = ""
+
+# reverse s2
+s2 = s2[::-1]
+
+# iterate string 
+# s1 ascending and s2 descending
+for i in range(length):
+    if i < s1_length:
+        result = result + s1[i]
+    if i < s2_length:
+        result = result + s2[i]
+
+print(result)
