fc4ce1804225f7fb51ae48dd402a15ff3a763e7f
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-07-12 19:29:22 +0530

    Refactor maxmarks.py for improved readability

--- maxmarks.py
+++ maxmarks.py
@@ -5,11 +5,7 @@ for i in range(n):
     m = int(input(f"Student {i+1}: "))
     marks.append(m)
 max_mark = marks[0]
-
-# compare each student's mark
 for i in range(1, n):
     if marks[i] > max_mark:
         max_mark = marks[i]
-
-# display result
 print("\nHighest mark in the class:", max_mark)
