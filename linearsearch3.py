9e9064538917716fa2bfa2bfa71d92e046e3925f
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-08-11 02:45:38 +0530

    Refactor linear search code for clarity

--- linearsearch3.py
+++ linearsearch3.py
@@ -7,8 +7,6 @@ for i in range(n):
     roll_numbers.append(roll)
 
 search_roll = int(input("Enter roll number to search: "))
-
-# Initialize a flag to indicate if roll number is found
 found = False
 
 # Linear Search
