44f797e05a3f95a9f446190541fef29d32677ac7
VINOD KUMAR VEMAGAL RAVIPRASAD <vinodkumarvinuchinnu2006@gmail.com>
2026-09-10 18:27:04 +0530

    Change rotation positions from 2 to a new value
    Updated the number of positions for array rotation.

--- array_rotation.py
+++ array_rotation.py
@@ -7,7 +7,6 @@ def rotate_array(arr, d):
     rotated_arr[i] = arr[(i + d) % n]
   return rotated_arr
 arr = [1, 2, 3, 4, 5]
-# Number of positions to rotate
 d = 2
 # Call the rotate_array function
 result = rotate_array(arr, d)
